76c8e0abe2b3e49cbcd140a8ab506658fe42c002
Biruk-Dereje-Getachew <biruk.scorpio.dereje@gmail.com>
2026-03-17 16:58:38 +0300

    Complete 08_removeFromArray successfully

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,7 +1,13 @@
 const removeFromArray = function (arr, ...toRemove) {
-  for (const itemToRemove of toRemove) {
-    const indexRemove = arr.indexOf(itemToRemove);
-    arr.splice(indexRemove, 1);
+  removedArray = [];
+
+  for (let j = 0; j < toRemove.length; j++) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === toRemove[j]) {
+        arr.splice(i, 1);
+        i--;
+      }
+    }
   }
   return arr;
 };
