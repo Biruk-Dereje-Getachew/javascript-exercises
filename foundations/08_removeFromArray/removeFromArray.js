2fc3e5d4306fc567c158b6434e536a30dc06f87a
Biruk-Dereje-Getachew <biruk.scorpio.dereje@gmail.com>
2026-03-17 16:41:31 +0300

    Write a solution that satisfies first two tests

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...toRemove) {
+  for (const itemToRemove of toRemove) {
+    const indexRemove = arr.indexOf(itemToRemove);
+    arr.splice(indexRemove, 1);
+  }
+  return arr;
 };
 
 // Do not edit below this line
