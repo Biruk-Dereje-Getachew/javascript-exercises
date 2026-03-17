8d22ae758f66828741b99c6eca653f8445dfbd6b
Biruk-Dereje-Getachew <biruk.scorpio.dereje@gmail.com>
2026-03-17 16:16:40 +0300

    Complete 07_reverseString successfully

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  letterArray = string.split("");
+  letterArray.reverse();
+  return letterArray.join("");
 };
 
 // Do not edit below this line
