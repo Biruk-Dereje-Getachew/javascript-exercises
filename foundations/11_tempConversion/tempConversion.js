c99bce608be5b4baba54c8390fdfa3bb34773b8b
Biruk-Dereje-Getachew <biruk.scorpio.dereje@gmail.com>
2026-03-18 10:38:42 +0300

    Complete 11_tempConversion successfully

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInFahreneit) {
+  let tempInCelsius = ((tempInFahreneit - 32) * 5) / 9;
+  tempInCelsius = Math.round(tempInCelsius * 10) / 10;
+  return tempInCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInCelsius) {
+  let tempInFahreneit = (9 * tempInCelsius) / 5 + 32;
+  tempInFahreneit = Math.round(tempInFahreneit * 10) / 10;
+  return tempInFahreneit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
